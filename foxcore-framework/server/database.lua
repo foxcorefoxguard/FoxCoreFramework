3472a3ca011f59ada698c4bd1033e58150f12785
foxcorefoxguard <foxcorefoxguard@gmail.com>
2025-02-26 23:01:45 -0500

    Delete foxcore-framework directory

--- foxcore-framework/server/database.lua
+++
@@ -1,9 +0,0 @@
-MySQL.ready(function()
-    Framework.Debug("Database connected successfully.")
-end)
-
-function SavePlayerData(player)
-    MySQL.Async.execute("INSERT INTO players (identifier) VALUES (@identifier) ON DUPLICATE KEY UPDATE last_seen=NOW()", {
-        ["@identifier"] = player.identifier
-    })
-end
