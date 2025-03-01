f97decd764fcc184aa3c4ff767e355519c7bc127
foxcorefoxguard <foxcorefoxguard@gmail.com>
2025-02-28 21:12:38 -0500

    Delete resources/foxcore_phone directory

--- resources/foxcore_phone/shared/config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.Phone = {
-    enableDarkWeb = true,  -- Enable Dark Web marketplace
-    startingBalance = 1000, -- Default bank balance
-    phonePrice = 500,       -- Cost of purchasing a new phone
-}
