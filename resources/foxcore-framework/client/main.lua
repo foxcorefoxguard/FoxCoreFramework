5227f10f992dea2d1b0d19176fa5710151e881f3
foxcorefoxguard <foxcorefoxguard@gmail.com>
2025-03-06 00:51:42 -0500

    new update

--- resources/foxcore-framework/client/main.lua
+++
@@ -1,4 +0,0 @@
-RegisterNetEvent("foxcore:initializePlayer", function(playerData)
-    Framework.Debug("Initializing player: " .. playerData.identifier)
-    SetEntityCoords(PlayerPedId(), Config.Framework.defaultSpawn)
-end)
